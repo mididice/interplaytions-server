fe9c1e794f257439263dd470eff6ee443d65fd6a
samdasu <full17@naver.com>
2018-10-07 20:43:06 +0900

    basic midi

--- test/test_api_key.py
+++ test/test_api_key.py
@@ -6,7 +6,7 @@ import io
 
 class ApiKeyTest(unittest.TestCase):
 	def test_api_1(self):
-		url = 'http://localhost:8000/api/midi/1/1/'
+		url = 'http://localhost:8000/api/midi/1/10/'
 		r = requests.post(url)
 		if r.status_code == 200:
 			self.assertTrue(r.text)
@@ -14,7 +14,7 @@ class ApiKeyTest(unittest.TestCase):
 			self.assertTrue(False)
 
 	def test_api_2(self):
-		url = 'http://localhost:8000/api/midi/2/2/'
+		url = 'http://localhost:8000/api/midi/2/8/'
 		r = requests.post(url)
 		if r.status_code == 200:
 			self.assertTrue(r.text)
@@ -22,7 +22,7 @@ class ApiKeyTest(unittest.TestCase):
 			self.assertTrue(False)
 
 	def test_api_3(self):
-		url = 'http://localhost:8000/api/midi/3/3/'
+		url = 'http://localhost:8000/api/midi/3/7/'
 		r = requests.post(url)
 		if r.status_code == 200:
 			self.assertTrue(r.text)
@@ -30,7 +30,7 @@ class ApiKeyTest(unittest.TestCase):
 			self.assertTrue(False)
 
 	def test_api_4(self):
-		url = 'http://localhost:8000/api/midi/4/4/'
+		url = 'http://localhost:8000/api/midi/4/9/'
 		r = requests.post(url)
 		if r.status_code == 200:
 			self.assertTrue(r.text)
@@ -41,7 +41,7 @@ class ApiKeyTest(unittest.TestCase):
 		url = 'http://localhost:8000/api/midi/combine/'
 		r = requests.post(url, stream=True)
 		if r.status_code == 200:
-			with io.open('generate_midi_0923.mid', 'wb') as f:
+			with io.open('generate_midi_1007.mid', 'wb') as f:
 				shutil.copyfileobj(r.raw, f)
 			self.assertTrue(f)
 		else:
